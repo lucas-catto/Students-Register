5b0caa04d26f3b23a43ccc1debc5aedeafba8d40
Lucas Silva <lucascattosilva2018@gmail.com>
2023-06-19 21:05:45 -0300

    'config.php' is done!

---
+++ config.php
@@ -0,0 +1,21 @@
+
+<style>html{background-color:gray;}</style>
+
+<html>
+    <pre>
+        <?php
+    
+            $host     = "localhost";
+            $dbname   = "School";
+            $username = "root";
+            $password = "130830";
+    
+            try{
+                $pdo = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
+            }
+            catch(PDOException $error){
+                echo "Error: " . $error;
+            }
+        ?>
+    </pre>
+</html>
