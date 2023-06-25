e48eb2cf2cf6436070670afb309a4bb61f2391d9
Lucas Silva <lucascattosilva2018@gmail.com>
2023-06-25 16:32:40 -0300

    Improving code of 'Student', correcting something that I didn't see before and initializing 'Teacher'.

--- Student/_index.php
+++ Student/_index.php
@@ -3,15 +3,15 @@
     require_once "../config.php"; 
     date_default_timezone_set('America/Sao_Paulo');
     
-    $name            = isset($_POST["name"])          ? $_POST["name"]          : "";
-    $registration    = isset($_POST["registration"])  ? $_POST["registration"]  : "";
-    $dateOfBirth     = isset($_POST["date_of_birth"]) ? $_POST["date_of_birth"] : "";
-    $momentRegister  = date("Y-m-d H:i:s");
+    if(isset($_POST["submit"])){
+        $name            = isset($_POST["name"])          ? $_POST["name"]          : "";
+        $registration    = isset($_POST["registration"])  ? $_POST["registration"]  : "";
+        $dateOfBirth     = isset($_POST["date_of_birth"]) ? $_POST["date_of_birth"] : "";
+        $momentRegister  = date("Y-m-d H:i:s");
 
-    $sql = $pdo->prepare("INSERT INTO `Students` VALUES (null,?,?,?,?)");
-    $sql->execute(array($name, $registration, $dateOfBirth, $momentRegister));
-    
-    if($sql->execute()){
+        $sql = $pdo->prepare("INSERT INTO `Students` VALUES (null,?,?,?,?)");
+        $sql->execute(array($name, $registration, $dateOfBirth, $momentRegister));
+        
         echo "Student ".$name." was registered!";
     }
 ?>
---
+++ Teacher/_index.php
@@ -0,0 +1,6 @@
+<?php
+    require_once "../config.php"; 
+    date_default_timezone_set('America/Sao_Paulo');
+
+    
+?>
